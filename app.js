bc4d76f65d9aaa9c7b8135214aa5aafe9f32513c
Codeliner <codeliner96@gmail.com>
2021-06-10 22:28:14 +0900

    Added login by username part.

---
+++ app.js
@@ -0,0 +1,34 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASS_NAME = "hidden";
+const GREETING_SAY = "Hello";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASS_NAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  setGreetings(username);
+}
+
+function setGreetings(username) {
+  greeting.innerText = `${GREETING_SAY} ${username}`;
+  greeting.classList.remove(HIDDEN_CLASS_NAME);
+}
+
+function checkLocalStorageIsEmpty() {
+  const username = localStorage.getItem(USERNAME_KEY);
+  console.log(username);
+  if (username) {
+    loginForm.classList.add(HIDDEN_CLASS_NAME);
+    setGreetings(username);
+  } else {
+    loginForm.classList.remove(HIDDEN_CLASS_NAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+  }
+}
+
+checkLocalStorageIsEmpty();
